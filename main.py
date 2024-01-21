d507cc324e7cd6e9b50de6b9af5724efd6c774d9
Max Shapira <max@winoutt.com>
2024-01-21 14:15:41 +0200

    implement /runs endpoint

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+
+from ci_server import CIDataManager
 
 app = FastAPI()
+data_manager = CIDataManager()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return Response("Server is running.")
+
+
+@app.get("/runs")
+def read_runs():
+    return data_manager.get_runs()
